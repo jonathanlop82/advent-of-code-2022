3efb918ecf9e443927612860d86fb848679f98dc
jonathanlop82 <jonathanlop82@gmail.com>
2022-12-02 09:22:12 -0500

    Ready day 2

---
+++ Day-2/part1.py
@@ -0,0 +1,48 @@
+
+
+'''
+    A for Rock, B for Paper, and C for Scissors
+
+    X for Rock, Y for Paper, and Z for Scissors
+
+    A Y
+    B X
+    C Z
+'''
+oponet_score = 0
+player_score = 0
+
+oponent = {
+    'A':[1,'Y','Z'],
+    'B':[2,'Z','X'],
+    'C':[3,'X','Y']
+
+}
+
+player = {
+    'X':[1,'B','C'],
+    'Y':[2,'C','A'],
+    'Z':[3,'A','B']
+}
+
+with open('list.text') as l:
+    lines = l.readlines()
+
+
+for line in lines:
+    op = line[0]
+    pl = line[2]
+    # If are de same value
+    if oponent[op][0] == player[pl][0]:
+        oponet_score = oponet_score + 3 + oponent[op][0]
+        player_score = player_score + 3 + player[pl][0]
+    else:
+        if oponent[op][1] == pl:
+            player_score = player_score + 6 + player[pl][0]
+            oponet_score = oponet_score + 0 + oponent[op][0]
+        elif oponent[op][2] == pl:
+            oponet_score = oponet_score + 6 + oponent[op][0]
+            player_score = player_score + 0 + player[pl][0]
+
+print(f'Oponet Score: {oponet_score}')
+print(f'Player Score: {player_score}')
