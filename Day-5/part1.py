faa160fd53f6845abf764602d41103c393b7d172
Jonathan Lopez <jonathanlop82@gmail.com>
2022-12-05 22:21:09 -0500

    Ready day 5

---
+++ Day-5/part1.py
@@ -0,0 +1,54 @@
+import re
+
+num_of_staks = 3
+
+staks1 = {
+    1:['J','H','P','M','S','F','N','V'],
+    2:['S','R','L','M','J','D','Q'],
+    3:['N','Q','D','H','C','S','W','B'],
+    4:['R','S','C','L'],
+    5:['M','V','T','P','F','B'],
+    6:['T','R','Q','N','C'],
+    7:['G','V','R'],
+    8:['C','Z','S','P','D','L','R'],
+    9:['D','S','J','V','G','P','B','F']
+}
+
+staks = {
+    1:['Z','N'],
+    2:['M','C','D'],
+    3:['P']
+}
+
+def print_staks():
+    for i in range(num_of_staks):
+        print(staks[i+1])
+
+with open('sample.text') as l:
+    lines = l.readlines()
+
+number_regex = re.compile('\d{1,2}')
+moves = []
+for line in lines:
+    line = line.replace('\n','')
+    first = number_regex.findall(line)
+    res= [eval(i) for i in first]
+    moves.append(res)
+
+
+
+print_staks()
+
+for move in moves:
+    #move 1
+    for i in range(move[0]):
+        letter = staks[move[1]][-1]
+        staks[move[1]].pop(-1)
+        staks[move[2]].append(letter)
+
+print('FINAL')
+print_staks()
+
+for j in range(num_of_staks):
+    print(f'{staks[j+1][-1]}',end ="")
+print()
